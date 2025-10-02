a12c5241f572fd52983b5a4d7024aa129066ce4a
Jm Torralvo <jm.torralvo@MacBook-Air-de-Jm.local>
2025-10-02 12:46:53 +0200

    chore(): remove unnecesary props in tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,30 +1,23 @@
 /// <reference types="node" />
-import { defineConfig } from "tsup";
 import { TsconfigPathsPlugin } from "@esbuild-plugins/tsconfig-paths";
-
-const { JAAQ_API_URL, NODE_ENV } = process.env;
-
-console.log("[build] NODE_ENV:", NODE_ENV, "JAAQ_API_URL:", JAAQ_API_URL);
+import { defineConfig } from "tsup";
 
 export default defineConfig({
-  define: {
-    "process.env.JAAQ_API_URL": JAAQ_API_URL
-      ? JSON.stringify(JAAQ_API_URL)
-      : "undefined",
-    "process.env.NODE_ENV": JSON.stringify(NODE_ENV ?? "production"),
-  },
   entry: ["src/index.ts"],
   outDir: "dist",
   dts: true,
   format: ["esm", "cjs"],
   sourcemap: true,
-  splitting: false,
-  clean: true,
   target: "es2020",
-  treeshake: true,
-  minify: false,
   platform: "neutral", // not asume node or browser.
-  skipNodeModulesBundle: true, // do not bundle node_modules
+  define: {
+    "process.env.JAAQ_API_URL": process.env.JAAQ_API_URL
+      ? JSON.stringify(process.env.JAAQ_API_URL)
+      : "undefined",
+    "process.env.NODE_ENV": JSON.stringify(
+      process.env.NODE_ENV ?? "production"
+    ),
+  },
   esbuildPlugins: [TsconfigPathsPlugin({})],
   outExtension({ format }) {
     return { js: format === "esm" ? ".mjs" : ".cjs" };
