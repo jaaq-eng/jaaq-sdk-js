b6f80efa0f3d7889ed7182f75a0972a280741199
Jm Torralvo <jm.torralvo@MacBook-Air-de-Jm.local>
2025-10-08 15:37:59 +0200

    feat(): update tests

--- tests/collections.test.ts
+++ tests/collections.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect, beforeEach } from "vitest";
 import { JaaqClient } from "@src/index";
-import collections from "@tests/mocks/collections.json";
-import collection from "@tests/mocks/collection.json";
+import collectionsResp from "@tests/mocks/collections.json";
+import collectionResp from "@tests/mocks/collection.json";
 
 describe("collections resource", () => {
   let sdk: JaaqClient;
@@ -16,13 +16,13 @@ describe("collections resource", () => {
 
   it("list returns collections", async () => {
     const res = await sdk.collections.list();
-    expect(res.length).toBe(collections.collections.length);
-    expect(res[0].id).toBe(collections.collections[0].id);
+    expect(res.length).toBe(collectionsResp.collections.length);
+    expect(res[0].id).toBe(collectionsResp.collections[0].id);
   });
 
   it("getById returns a single collection", async () => {
-    const res = await sdk.collections.getById(collection.collection.id);
-    expect(res.id).toBe(collection.collection.id);
-    expect(res.name).toBe(collection.collection.name);
+    const res = await sdk.collections.getById(collectionResp.collection.id);
+    expect(res.id).toBe(collectionResp.collection.id);
+    expect(res.name).toBe(collectionResp.collection.name);
   });
 });
--- tests/videos.test.ts
+++ tests/videos.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { JaaqClient } from "@src/index";
-import videos from "@tests/mocks/videos.json";
+import videoResp from "@tests/mocks/video.json";
 
 describe("videos resource", () => {
   it("getById returns a single video", async () => {
@@ -10,8 +10,8 @@ describe("videos resource", () => {
       clientId: "acme",
     });
 
-    const res = await sdk.videos.getById(videos.video.id);
-    expect(res.id).toBe(videos.video.id);
-    expect(res.videoId).toBe(videos.video.videoId);
+    const res = await sdk.videos.getById(videoResp.video.id);
+    expect(res.id).toBe(videoResp.video.id);
+    expect(res.videoId).toBe(videoResp.video.videoId);
   });
 });
