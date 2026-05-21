115b8f85970596726c364c01218d8286e0b7fa3b
Viktor Illes <viktor.illes@one-beyond.com>
2026-05-21 12:06:15 +0200

    feat(B2B-345): use new subtitle dto

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,3 +8,5 @@ export type { VideoV2DTO } from './v2/videos';
 export type { CollectionV2DTO } from './v2/collection';
 export type { VideoGroupV2DTO } from './v2/videoGroup';
 export type { CreatorV2DTO } from './v2/creator';
+export type { CtaV2DTO } from './v2/cta';
+export type { SubtitleV2DTO } from './v2/subtitle';
