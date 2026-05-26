6918f2d4796461f6840ee85182c356beb4929b0d
ViktorIlles <53177786+ViktorIlles@users.noreply.github.com>
2026-05-26 09:28:59 +0200

    feat(B2B-345): use new subtitle dto (#38)

---
+++ src/types/v2/subtitle.ts
@@ -0,0 +1,7 @@
+type SubtitleV2DTO = {
+  code: string;
+  url: string;
+  default?: boolean;
+};
+
+export type { SubtitleV2DTO };
--- src/types/v2/videos.ts
+++ src/types/v2/videos.ts
@@ -1,21 +1,17 @@
 import type { CreatorV2DTO } from './creator';
-
-type CtaDTO = {
-  url?: string;
-  label?: string;
-  labelColor?: string;
-};
+import type { CtaV2DTO } from './cta';
+import type { SubtitleV2DTO } from './subtitle';
 
 type VideoV2DTO = {
   id: string;
   videoId: string;
   question: string;
   transcription: string;
-  subtitle: string;
+  subtitles: SubtitleV2DTO[];
   videoUrl: string;
   tags: string[];
   severity: number | null;
-  cta: CtaDTO | null;
+  cta: CtaV2DTO | null;
   duration: number;
   description: string;
   creator?: CreatorV2DTO;
